1ba398c7942c4e37f98516ea99c02d3a8ef6f680
Tom McFarlin <tom@tommcfarlin.com>
2010-09-21 15:29:01 -0400

    added ajax functionality, drop shadows, and styles

--- js/jquery.slidenote.js
+++ js/jquery.slidenote.js
@@ -5,33 +5,58 @@
 		var opts = $.extend($.fn.slideNote.defaults, options);
 		return this.each(function() {
 			
-			var $note = $('#note').toggle().width(0);
+			var $note = $(this).toggle()
+									.css(opts.corner, -1 * $(this).width());
+			
+			if(opts.url !== null) {
+				_retrieveData($note, opts);
+			}
 			
 			$(document).scroll(function() {
-
-				if($(this).scrollTop() >= $.fn.slideNote.defaults.where) {
+				if($(this).scrollTop() >= opts.where) {
 					if(!$note.is(':visible')) {
-						$note.show().animate({
-							width: 640
-						}, 1000);
+						_slideIn($note, opts);
 					}
-				} else if ($(this).scrollTop() < $.fn.slideNote.defaults.where) {
+				} else if ($(this).scrollTop() < opts.where) {
 					if($note.is(':visible')) {
-						$note.animate({
-							width: 0
-						}, 1000, function() {
-							$(this).stop(true).hide();
-						});
+						_slideOut($note, opts);
 					}
 				}
-				
 			});
 			
 		});
+		
+	};
+	
+	function _slideIn($obj, opts) {	
+		var direction = opts.corner === 'right' ? { 'right' : 0 } : { 'left' : 0 } ;
+		$obj.show().animate(direction, 1000, 'swing');
+	};
+	
+	function _slideOut($obj, opts) {
+		var direction = opts.corner === 'right' ? { 'right' : -1 * $obj.width() } : { 'left' : -1 * $obj.width() }
+		$obj.animate(direction, 1000, 'swing', function() {
+			$(this).stop(true).hide();
+		});
+	};
+	
+	function _retrieveData($obj, opts) {
+		
+		if(opts.container.indexOf('#') === 0) {
+			opts.container = opts.container.substring(1, opts.container.length);
+		}
+		
+		$obj.load(opts.url + ' ' + opts.container, function(data) {
+			$(this).append(data);
+		});
+		
 	};
 
 	$.fn.slideNote.defaults = {
-		where: 640
+		where: 640,
+		corner: 'right',
+		url: null,
+		container: ''
 	}
 	
 })(jQuery);
